673aff1554baed96b8b3f81fcfdde8196479d50e
Joseph Luck <joseph.luck@sky.com>
2016-10-22 12:53:03 +0100

    Adding transition to breadcrumbs

--- src/containers/Breadcrumbs.vue
+++ src/containers/Breadcrumbs.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="breadcrumbs">
-    <span
-      v-for="(breadcrumb, index) in breadcrumbs"
-      class="breadcrumb"
-    >
-      <router-link
-        v-if="breadcrumb.to"
-        v-bind:to="breadcrumb.to"
-      >
-        {{ breadcrumb.name }}
-      </router-link>
+    <transition-group name="breadcrumb" tag="div">
       <span
-        v-else
-        class="gray"
+        v-for="(breadcrumb, index) in breadcrumbs"
+        v-bind:key="index"
+        class="breadcrumb"
       >
-        {{ breadcrumb.name }}
+        <span
+          v-if="index !== 0"
+          class="icon ss-navigateright light-silver ph1"
+        ></span>
+        <router-link
+          v-if="breadcrumb.to"
+          v-bind:to="breadcrumb.to"
+        >
+          {{ breadcrumb.name }}
+        </router-link>
+        <span
+          v-else
+          class="gray"
+        >
+          {{ breadcrumb.name }}
+        </span>
       </span>
-      <span
-        v-if="index !== breadcrumbs.length - 1"
-        class="icon ss-navigateright light-silver ph1"
-      ></span>
-    </span>
+    </transition-group>
   </div>
 </template>
 
