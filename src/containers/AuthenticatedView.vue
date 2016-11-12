2924acb5ed6863225b5ef2abd8ed14e2f4b3472e
Joseph Luck <joseph.luck@sky.com>
2016-11-12 15:02:15 +0000

    Adding top bar with logo

--- src/containers/AuthenticatedView.vue
+++ src/containers/AuthenticatedView.vue
@@ -1,52 +1,60 @@
 <template>
-  <div class="flex h-100">
-    <div class="bg-white w-20 br b--black-10 flex flex-column overflow-auto">
-      <div class="flex-1 pa3">
-        <projects-list></projects-list>
-        <a class="db mb3 mt3" v-on:click="newProjectFormShowing = true">
-          New project
-        </a>
-        <a class="db" v-on:click.prevent="logout">
-          Logout
-        </a>
+  <div class="flex flex-column h-100">
+    <div class="flex items-center bg-white ph4 pv3 bb b--black-10">
+      <div class="flex-1">
+        <span class="icon blue ss-compasstool"></span>
+        <span class="ttu tracked ml1 fw5 blue">Blueprint</span>
       </div>
+      <a class="db ml3" v-on:click="newProjectFormShowing = true">
+        New project
+      </a>
+      <a class="db ml3" v-on:click.prevent="logout">
+        Logout
+      </a>
     </div>
-    <div class="flex flex-column flex-1 pt3 overflow-auto">
-      <transition v-bind:name="transitionName" mode="out-in">
-        <router-view class="child-view"></router-view>
-      </transition>
+    <div class="flex flex-1 h-100 overflow-hidden">
+      <div class="mt1 flex flex-column overflow-auto">
+        <div class="flex-1 pa3 pl4">
+          <projects-list></projects-list>
+        </div>
+      </div>
+      <div class="flex flex-column flex-1 pt3 overflow-auto">
+        <transition v-bind:name="transitionName" mode="out-in">
+          <router-view class="child-view"></router-view>
+        </transition>
+      </div>
+
+      <form v-on:submit.prevent="saveNewProject">
+        <modal v-if="newProjectFormShowing"
+          v-on:close="newProjectFormShowing = false">
+          <div slot="header"
+            class="pa3 bb b--black-20 bg-white">
+            New project
+          </div>
+          <div slot="content"
+            class="pa3">
+            <label class="dib w-100 mb1">
+              Project name
+            </label>
+            <input class="w-100"
+              v-model="newProjectName" />
+          </div>
+
+          <div slot="footer"
+            class="pa3 flex items-center bt b--black-20 bg-white">
+            <div class="flex-1"></div>
+            <a v-on:click.prevent="newProjectFormShowing = false"
+              class="mr3">
+              Close
+            </a>
+            <button type="submit"
+              class="button">
+              Save
+            </button>
+          </div>
+        </modal>
+      </form>
     </div>
-
-    <form v-on:submit.prevent="saveNewProject">
-      <modal v-if="newProjectFormShowing"
-        v-on:close="newProjectFormShowing = false">
-        <div slot="header"
-          class="pa3 bb b--black-20 bg-white">
-          New project
-        </div>
-        <div slot="content"
-          class="pa3">
-          <label class="dib w-100 mb1">
-            Project name
-          </label>
-          <input class="w-100"
-            v-model="newProjectName" />
-        </div>
-
-        <div slot="footer"
-          class="pa3 flex items-center bt b--black-20 bg-white">
-          <div class="flex-1"></div>
-          <a v-on:click.prevent="newProjectFormShowing = false"
-            class="mr3">
-            Close
-          </a>
-          <button type="submit"
-            class="button">
-            Save
-          </button>
-        </div>
-      </modal>
-    </form>
   </div>
 </template>
 
